fbcea444c9f1c613040ba775c5272d55b40a02bb
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2016-07-02 17:16:31 +0000

    Refactoring.

--- plugins/YumUpdate/lib/MT/Plugin/YumUpdate.pm
+++ plugins/YumUpdate/lib/MT/Plugin/YumUpdate.pm
@@ -5,17 +5,31 @@ use warnings;
 use MT;
 
 sub code {
-    my @logs = `sudo yum -y update 2>&1`;
-    my $log = join "\n", @logs;
-    if ($log !~ /No Packages marked for update/m) {
-        MT->log(
-            {   message  => 'Updated yum packages',
-                category => 'yum update',
-                metadata => $log,
-            }
-        );
+    my $log = _yum_update();
+    if ( _packages_were_updated($log) ) {
+        _write_log($log);
     }
     1;
+}
+
+sub _yum_update {
+    my @logs = `sudo yum -y update 2>&1`;
+    join "\n", @logs;
+}
+
+sub _packages_were_updated {
+    my $log = shift;
+    $log =~ /No Packages marked for update/m ? 0 : 1;
+}
+
+sub _write_log {
+    my $log = shift;
+    MT->log(
+        {   message  => 'Updated yum packages',
+            category => 'yum update',
+            metadata => $log,
+        }
+    );
 }
 
 1;
