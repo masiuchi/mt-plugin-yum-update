7d11085c6131efb3f5e752d854717b0486e35719
Masahiro Iuchi <masahiro.iuchi@gmail.com>
2016-07-03 03:44:53 +0000

    _superuser_exists.

--- plugins/YumUpdate/lib/MT/Plugin/YumUpdate.pm
+++ plugins/YumUpdate/lib/MT/Plugin/YumUpdate.pm
@@ -13,7 +13,7 @@ sub code {
     $yum_update->run;
     if ( $yum_update->is_updated ) {
         _write_log( $yum_update->log );
-        if ( my $su = &_exist_superuser ) {
+        if ( my $su = &_superuser_exists ) {
             MT::Plugin::YumUpdate::Mail->send( $su->email, $yum_update->log );
         }
     }
@@ -30,7 +30,7 @@ sub _write_log {
     );
 }
 
-sub _exist_superuser {
+sub _superuser_exists {
     my $iter = MT::Author->load_iter( undef, { sort => 'id' } );
     while ( my $user = $iter->() ) {
         return $user if $user->is_superuser;
